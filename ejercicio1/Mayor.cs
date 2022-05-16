98a24be45f1d7417969940295cfa7e610382a3a2
josemeroq <93845854+josemeroq@users.noreply.github.com>
2022-05-15 20:59:35 -0500

    Add files via upload

---
+++ ejercicio1/Mayor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JoseMero3A_E1
+{
+    public class Mayor
+    {
+        private int valorA { set; get; }
+        private int valorB { set; get; }
+
+        public Mayor(int valorA, int valorB)
+        {
+            this.valorA = valorA;
+            this.valorB = valorB;
+        }
+        public void proceso()
+        {    
+            if (valorA > valorB) {   
+                Console.WriteLine("El numero " + valorA + " es mayor");
+            }
+            else
+            {
+                Console.WriteLine("El numero " + valorB + " es mayor");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ejercicio1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace JoseMero3A_E1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mayor mayorn = new Mayor(6,8);
+            mayorn.proceso();
+        }
+
+    }
+}
